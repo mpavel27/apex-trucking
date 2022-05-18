9a11c9d94d5ec7f6ff3d5e8e25798468c11d0835
Kraier <kraiermarin11@gmail.com>
2022-05-18 01:05:56 +0100

    Added csrf token and client-side validation

--- apex-website/components/ApplyForm.vue
+++ apex-website/components/ApplyForm.vue
@@ -7,39 +7,120 @@
         <div class="px-4 py-5 bg-white sm:p-6">
           <div class="grid grid-cols-6 gap-6">
             <div class="col-span-6">
-              <label for="username" class="block text-sm font-medium text-gray-700">Username</label>
-              <input type="text" name="username" id="username" class="mt-1 border-2 focus:border-green-400 w-full shadow-sm sm:text-sm border-gray-200 rounded-md outline-none px-3 py-3" />
+              <ValidationProvider rules="required" v-slot="{ errors }">
+                <label for="username" class="block text-sm font-medium text-gray-700">Username</label>
+                <input type="text" v-model="username" name="username" id="username" class="mt-1 border-2 focus:border-green-400 w-full shadow-sm sm:text-sm border-gray-200 rounded-md outline-none px-3 py-3" required />
+                <span class="text-red-600">{{ errors[0] }}</span>
+              </ValidationProvider>
             </div>
             <div class="col-span-6">
-              <label for="email" class="block text-sm font-medium text-gray-700">Adresa E-mail</label>
-              <input type="text" name="email" id="email" class="mt-1 border-2 focus:border-green-400 block w-full shadow-sm sm:text-sm border-gray-200 rounded-md outline-none px-3 py-3" />
+              <ValidationProvider rules="required|email" v-slot="{ errors }">
+                <label for="email" class="block text-sm font-medium text-gray-700">Adresa E-mail</label>
+                <input type="text" v-model="email" name="email" id="email" class="mt-1 border-2 focus:border-green-400 block w-full shadow-sm sm:text-sm border-gray-200 rounded-md outline-none px-3 py-3" required />
+                <span class="text-red-600">{{ errors[0] }}</span>
+              </ValidationProvider>
+            </div>
+              <div class="col-span-6">
+                <ValidationProvider vid="password" rules="required" v-slot="{ errors }">
+                  <label for="password" class="block text-sm font-medium text-gray-700">Parola</label>
+                  <input type="password" v-model="password" name="password" id="password" class="mt-1 border-2 focus:border-green-400 block w-full shadow-sm sm:text-sm border-gray-200 rounded-md outline-none px-3 py-3" required />
+                  <span class="text-red-600">{{ errors[0] }}</span>
+                </ValidationProvider>
+              </div>
+            <div class="col-span-6">
+              <ValidationProvider vid="confirmPassword" rules="required|matchWith:@password" v-slot="{ errors }">
+                <label for="repassword" class="block text-sm font-medium text-gray-700">Repetă Parola</label>
+                <input type="password" v-model="repassword" name="repassword" id="repassword" class="mt-1 border-2 focus:border-green-400 block w-full shadow-sm sm:text-sm border-gray-200 rounded-md outline-none px-3 py-3" required />
+                <span class="text-red-600">{{ errors[0] }}</span>
+              </ValidationProvider>
             </div>
             <div class="col-span-6">
-              <label for="age" class="block text-sm font-medium text-gray-700">Varstă</label>
-              <input v-model="age" type="number" name="age" id="age" class="mt-1 border-2 focus:border-green-400 block w-full shadow-sm sm:text-sm border-gray-200 rounded-md outline-none px-3 py-3" />
-              <p>{{ age }}</p>
+              <ValidationProvider rules="required|ageValidation" v-slot="{ errors }">
+                <label for="age" class="block text-sm font-medium text-gray-700">Varstă</label>
+                <input v-model="age" type="number" name="age" id="age" class="mt-1 border-2 focus:border-green-400 block w-full shadow-sm sm:text-sm border-gray-200 rounded-md outline-none px-3 py-3" required />
+                <span class="text-red-600">{{ errors[0] }}</span>
+              </ValidationProvider>
             </div>
             <div class="col-span-6">
-              <label for="truckersmp_id" class="block text-sm font-medium text-gray-700">TruckersMP ID</label>
-              <input type="text" name="truckersmp_id" id="truckersmp_id" class="mt-1 border-2 focus:border-green-400 block w-full shadow-sm sm:text-sm border-gray-200 rounded-md outline-none px-3 py-3" />
+              <ValidationProvider rules="required" v-slot="{ errors }">
+                <label for="truckersmp_id" class="block text-sm font-medium text-gray-700">TruckersMP ID</label>
+                <input type="text" v-model="tmpid" name="truckersmp_id" id="truckersmp_id" class="mt-1 border-2 focus:border-green-400 block w-full shadow-sm sm:text-sm border-gray-200 rounded-md outline-none px-3 py-3" required />
+                <span class="text-red-600">{{ errors[0] }}</span>
+              </ValidationProvider>
             </div>
             <div class="col-span-6">
-              <label for="question1" class="block text-sm font-medium text-gray-700">De ce vrei să te alaturi companiei?</label>
-              <textarea name="question1" id="question1" class="mt-1 border-2 focus:border-green-400 block w-full shadow-sm sm:text-sm border-gray-200 rounded-md outline-none px-3 py-3" />
+              <ValidationProvider rules="required|questionLength:100" v-slot="{ errors }">
+                <label for="question1" class="block text-sm font-medium text-gray-700">De ce vrei să te alaturi companiei? <span class="text-gray-400">(Minim: 100 caractere)</span></label>
+                <textarea name="question1" v-model="question1" id="question1" class="mt-1 border-2 focus:border-green-400 block w-full shadow-sm sm:text-sm border-gray-200 rounded-md outline-none px-3 py-3" required />
+                <span class="text-red-600">{{ errors[0] }}</span>
+              </ValidationProvider>
             </div>
           </div>
         </div>
         <div class="px-4 py-3 bg-gray-50 text-right sm:px-6">
-          <button type="submit" class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-black btn focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Aplică acum</button>
+          <button type="button" class="inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-black btn focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">Aplică acum</button>
         </div>
       </div>
     </form>
   </div>
 </template>
 
-<script>
+<script type="module">
+import { ValidationObserver, ValidationProvider, extend } from 'vee-validate';
+import { required, email } from 'vee-validate/dist/rules';
+
+extend('ageValidation', {
+  validate(value) {
+    if(value >= 16) {
+      return true;
+    }
+  },
+  message: '* Trebuie să ai varsta minimă de 16 ani'
+});
+
+extend('questionLength', {
+  params: ['length'],
+  validate(value, { length }) {
+    if(value.length >= length)
+      return true;
+  },
+  message: '* Răspunsul la întrebare trebuie să aibe minim 100 de caractere'
+})
+
+extend('email', {
+  ...email,
+  message: '* Acest e-mail trebuie să fie unul valid'
+})
+
+extend('required', {
+  ...required,
+  message: '* Acest câmp este obligatoriu'
+});
+
+extend('matchWith', {
+  params: ['target'],
+  validate(value, { target }) {
+    return value === target;
+  },
+  message: '* Parolele nu corespund între ele'
+});
+
 export default {
-  name: "ApplyForm"
+  name: "ApplyForm",
+  components: {
+    ValidationProvider,
+    ValidationObserver
+  },
+  data() {
+    return {
+      username: '',
+      password: '',
+      repassword: '',
+      email: '',
+      age: '',
+      question1: '',
+    }
+  }
 }
 </script>
 
--- server.js
+++ server.js
@@ -1,25 +1,23 @@
 import { PrismaClient } from '@prisma/client';
 import express from 'express';
 import cors from 'cors';
+import cookieParse from 'cookie-parser';
+import csrf from 'csurf';
 
 const app = express();
 const prisma = new PrismaClient()
+const corsOptions = { origin: "http://localhost:3000/", credentials: true };
+let csrfProtection = csrf({ cookie: true });
 
-app.use(cors());
+app.use(cors(corsOptions));
+app.use(cookieParse());
+
+app.get('/api/csrf', csrfProtection, function(req, res) {
+    return res.json({csrfToken: req.csrfToken()});
+});
 
 app.get('/greeting',(req,res)=>{
     res.json({greeting:'Hello'})
 })
-
-app.get(`/test`, async (req, res) => {
-    const result = await prisma.users.create({
-        data: {
-            username: "test",
-            password: "test2",
-        },
-    })
-    res.json(result);
-})
-
 
 app.listen(5000,()=>console.log('server started'))
